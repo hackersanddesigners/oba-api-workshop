0cf1ccf34914c86f35819f6b2801d97e6c0ab467
afincato <work@andrefincato.info>
2020-02-08 15:32:04 +0100

    add range option to fetch set amount of pages

--- oba_pub_search.py
+++ oba_pub_search.py
@@ -14,50 +14,19 @@ load_dotenv(dotenv_path)
 
 # your oba_api_key gets loaded from the .env file in settings.py
 
-# the query parameters: (update according to your search query)
-q = "hacking"
-pagesize = 1
+for i in range(int(sys.argv[2])):
+  query = "https://zoeken.oba.nl/api/v1/search/?authorization=" + oba_api_key + "&q=publisher%3A" + sys.argv[1] + "&page=" + str(i)
+  result = requests.get(query)
+  data = result.text
+  data = xmltodict.parse(data)['aquabrowser']
 
-query = "https://zoeken.oba.nl/api/v1/search/?authorization=" + oba_api_key + "&q=publisher%3A" + sys.argv[1]
-result = requests.get(query)
-data = result.text
-data = xmltodict.parse(data)
+  result_index.append(data)
 
 print(json.dumps(data, indent=4, sort_keys=True))
 print('---')
 
 timestamp = time.strftime("%Y-%m-%d-%H%M%S")
 filename = sys.argv[1] + '_' + timestamp
-
 with open('dump/%s.json' % filename, 'w') as fp:
-  json.dump(data, fp)
-  print('dumped to json!')
-
-# for k,v in data['aquabrowser']['results'].items():
-#   # print(json.dumps(v, indent=4, sort_keys=True))
-#   if 'titles' in v:
-#     title = v['titles']['title']['#text']
-#     print('title: ' + title + '\n')
-# 
-#     cover = v['coverimages']['coverimage']
-#     print('cover:')
-#     for img in cover:
-#       print('  → ' + img['#text'])
-# 
-#     print('\n--\n')
-#   else:
-#     for entry in v:
-#       title = entry['titles']['title']
-#       if '#text' in title:
-#         print('title: ' + title['#text'] + '\n')
-#       else:
-#         for t in title:
-#           print('title: ' + t['#text'] + '\n')
-# 
-#       cover = entry['coverimages']['coverimage']
-#       # print(cover)
-#       print('cover:')
-#       for img in cover:
-#         print('  → ' + img['#text'])
-# 
-#       print('\n--\n')
+    json.dump(result_index, fp)
+    print('dumped to json!')
