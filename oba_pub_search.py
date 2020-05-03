b5030fc8c2731246693ab042156ce6c59c75bcbf
afincato <work@andrefincato.info>
2020-05-03 16:15:48 +0200

    clean up and return list of all records in one array

--- oba_pub_search.py
+++ oba_pub_search.py
@@ -1,30 +1,29 @@
-# search for a book in the oba api
-from settings import *
-from os.path import join, dirname
-from dotenv import load_dotenv
+# -- search for a book in the oba api
+import os
 import sys
 import requests
 import xmltodict
 import json
 import time
+import settings
 
-# get the api keys from the .env file
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-# your oba_api_key gets loaded from the .env file in settings.py
+# -- get dict of python environment variables loaded from `.env`
+env = settings.env()
 
 result_index = []
 for i in range(1, int(sys.argv[2])):
-  query = "https://zoeken.oba.nl/api/v1/search/?authorization=" + oba_api_key + "&q=publisher%3A" + sys.argv[1] + "&page=" + str(i)
+  query = "https://zoeken.oba.nl/api/v1/search/?authorization=" + env['oba_api_key'] + "&q=publisher%3A" + sys.argv[1] + "&page=" + str(i)
   result = requests.get(query)
   data = result.text
   data = xmltodict.parse(data)['aquabrowser']
 
-  result_index.append(data)
+  for k, v in data.items():
+    if (k == 'results'):
+      for item in v['result']:
+        result_index.append(item)
 
-print(json.dumps(data, indent=4, sort_keys=True))
-print('---')
+# print(json.dumps(data, indent=4, sort_keys=True))
+# print('---')
 
 timestamp = time.strftime("%Y-%m-%d-%H%M%S")
 filename = sys.argv[1] + '_' + timestamp
