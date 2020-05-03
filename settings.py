7cd2a9b95d429c88a722310559627ea941841b16
afincato <work@andrefincato.info>
2020-05-03 16:15:29 +0200

    simplify settings to return list of os.env variables

--- settings.py
+++ settings.py
@@ -1,16 +1,12 @@
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
+load_dotenv()
 
-# Create .env file path.
-dotenv_path = join(dirname(__file__), '.env')
 
-# Load file from the path.
-load_dotenv(dotenv_path)
+def env():
+  dict = {'oba_api_key': os.getenv('oba_api_key'),
+          'rapidapi_key': os.getenv('rapidapi_key'),
+          'imagga_api_key': os.getenv('imagga_api_key'),
+          'imagga_api_secret': os.getenv('imagga_api_secret')}
 
-oba_api_key = os.getenv('oba_api_key')
-rapidapi_key = os.getenv('rapidapi_key')
-imagga_api_key = os.getenv('imagga_api_key')
-imagga_api_secret = os.getenv('imagga_api_secret')
-# MS_Azure_Key = os.getenv('MS_Azure_Key')
-# MS_Azure_Image_Key = os.getenv('MS_Azure_Image_Key')
+  return dict
